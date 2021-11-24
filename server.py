6b7cc705a6b67132d77582ff3205bac4a40b4796
Eliaou Azulai <eliaou.re@gmail.com>
2021-11-24 14:08:41 +0200

    Initial commit

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from flask import request
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    return 'form submitted'
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
